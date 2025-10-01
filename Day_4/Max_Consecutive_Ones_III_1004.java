1580094790d393dce7e6fa258a3861197aba884a
Arun Kushwah <arunkushwah592005@gmail.com>
2025-10-01 14:22:01 +0530

    Day-4

---
+++ Day_4/Max_Consecutive_Ones_III_1004.java
@@ -0,0 +1,38 @@
+package LEETCODE;
+
+public class Max_Consecutive_Ones_III_1004 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums = { 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1 };
+		int k = 3;
+		System.out.println(longestOnes(nums, k));
+	}
+
+	public static int longestOnes(int[] nums, int k) {
+		int si = 0;
+		int ei = 0;
+		int flip = 0;
+		int ans = 0;
+
+		while (ei < nums.length) {
+
+			if (nums[ei] == 0) {
+				flip++;
+			}
+
+			while (flip > k && si <= ei) {
+				if (nums[si] == 0) {
+					flip--;
+				}
+				si++;
+			}
+
+			ans = Math.max(ans, ei - si + 1);
+			ei++;
+
+		}
+		return ans;
+	}
+
+}
---
+++ Day_4/Permutation_in_String_567.java
@@ -0,0 +1,52 @@
+package LEETCODE;
+
+import java.util.*;
+
+public class Permutation_in_String_567 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "abc", s2 = "eidbcaooo";
+		System.out.println(checkInclusion(s1, s2));
+	}
+
+	public static boolean checkInclusion(String s1, String s2) {
+
+		if (s1.length() > s2.length()) {
+			return false;
+		}
+
+		HashMap<Character, Integer> map = new HashMap<>();
+		for (char ch : s1.toCharArray()) {
+			map.put(ch, map.getOrDefault(ch, 0) + 1);
+		}
+		int c = 0;
+		int si = 0, ei = 0;
+		while (ei < s2.length()) {
+			if (!map.containsKey(s2.charAt(ei))) {
+				while (si < ei) {
+					map.put(s2.charAt(si), map.get(s2.charAt(si)) + 1);
+					si++;
+					c--;
+				}
+				si++;
+				ei++;
+			} else {
+				while (si < ei && map.get(s2.charAt(ei)) == 0) {
+					map.put(s2.charAt(si), map.get(s2.charAt(si)) + 1);
+					si++;
+					c--;
+				}
+				map.put(s2.charAt(ei), map.get(s2.charAt(ei)) - 1);
+				ei++;
+				c++;
+				if (c == s1.length()) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+}
